5fbae2193ba35ecccf0b825daf89932923f88108
Rwik2000 <rwik.rana@iitgn.ac.in>
2021-04-25 00:50:26 +0530

    openloop part 1 done

--- openloop_analysis.m
+++ openloop_analysis.m
@@ -29,12 +29,12 @@ B = [0 0;
      l/I_xx -l/I_xx;];
 
 C = [1 0 0 0 0 0; 
-      0 0 1 0 0 0; 
-      0 0 0 0 1 0];
+     0 0 1 0 0 0; 
+     0 0 0 0 1 0];
   
 D =[0 0; 
-     0 0; 
-     0 0];%zero matrix shape: 3x2
+    0 0; 
+    0 0];%zero matrix shape: 3x2
 
 % Phi=inv(s*eye(6)-A);
 % H=C*Phi*B+D
@@ -45,10 +45,20 @@ D =[0 0;
  
 % direct with numerical values
 %%%%%%%%%%%%%%%%%%%%%%%%%% 
- sys = ss(A,B,C,D);
- sys_as_tf = tf(sys);
-step(sys_as_tf)
-[y_OL,t]=step(sys_as_tf);
-step(sys_as_tf)
+%  sys = ss(A,B,C,D);
+%  sys_as_tf = tf(sys);
+% step(sys_as_tf);
+% [y_OL,t]=step(sys_as_tf);
+% step(sys_as_tf);
 % xlim([0 10])
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sys = ss(A,B,C,D);
+sys_as_tf = tf(sys);
+t = 0:0.04:10;
+F1 = ones(length(t),1);
+F2 = ones(length(t),1);
+
+% F1 = 1;
+% F2 = 1;
+lsim(sys,[F1 F2],t)
+
