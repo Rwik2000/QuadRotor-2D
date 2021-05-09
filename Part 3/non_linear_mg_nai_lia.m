c799caf2044a9cb862a05a327e232f70e2fa8bd7
SleepyJoker15 <prankush.agarwal@iitgn.ac.in>
2021-05-10 00:01:01 +0530

    part 3 try

--- Part 3/non_linear_mg_nai_lia.m
+++ Part 3/non_linear_mg_nai_lia.m
@@ -50,14 +50,17 @@ for i = 2:length(t)-1
     
     del_z = z(i) - z(i-1);
     del_y = y(i) - y(i-1);
-    u1 = m*(g + 0 +kd(2)*(0 - del_z/del_t) + kp(2)*(z_des - z(i)));
-    phi_c = -1/g*(0 + kd(1)*(0 - del_y/del_t) + kp(1)*(y_des - y(i)));   
+    u1cosphi = m*(g + 0 +kd(2)*(0 - del_z/del_t) + kp(2)*(z_des - z(i)));
+    u1sinphi = -m*(0 + kd(1)*(0 - del_y/del_t) + kp(1)*(y_des - y(i)));   
+    
+    u1 = (u1cosphi^2+u1sinphi^2)^0.5;
+    phi_c = atan(u1sinphi/u1cosphi);
     
     del_phidot = (phi_c - phi(i-1))/del_t - (phi(i)-phi(i-1))/del_t;
     u2 = I_xx*(0 + kd(3)*(del_phidot) + kp(3)*(phi_c - phi(i)));
     
-    yd_dot = -g*phi_c;
-    zd_dot = -g +u1/m;
+    yd_dot = -(u1/m)*sin(phi_c);
+    zd_dot = -g +u1*cos(phi_c)/m;
     phid_dot = u2/I_xx;
     
     y(i+1) = y(i) + ydot*del_t + 0.5*yd_dot*(del_t^2);
@@ -65,7 +68,7 @@ for i = 2:length(t)-1
     
     z(i+1) = z(i) + zdot*del_t + 0.5*zd_dot*(del_t^2);
     zdot = zdot + zd_dot*del_t;
-    
+   
     
     phi(i+1) = phi(i) + phidot*del_t + 0.5*phid_dot*(del_t^2);
     phidot = phidot + phid_dot*del_t;
