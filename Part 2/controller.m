819405e42cc308873394e20febbd4f9152cc6eb6
Rwik2000 <rwik.rana@iitgn.ac.in>
2021-05-09 15:35:07 +0530

    some changes

--- Part 2/controller.m
+++ Part 2/controller.m
@@ -1,8 +1,8 @@
 clc;
 clear;
 
-kp = [20, 80, 1000];
-kd = [5, 20, 10];
+kp = [0, 0.01, 0];
+kd = [0, 0, 0];
 ki = [0, 0, 0];
 % kphi = [0, 0, 0];
 % kp = [kp_y, kp_z, kp_phi];
@@ -23,8 +23,8 @@ ydot = 0;
 zdot = 0;
 phidot = 0;
 
-y_des = 7;
-z_des = 12;
+y_des = 0;
+z_des = 1;
 phi_des = 0;
 
 t=0:del_t:100;
@@ -39,14 +39,32 @@ phi(1) = phi_init;
 
 length(t);
 
-for i = 1:length(t)-1
-    del_z = z_des - z(i); 
-    del_y = y_des - y(i);
-    del_phi = phi_des - phi(i);
+prev_z = 0;
+prev_phi_c = 0;
+for i = 2:length(t)-1
+%     del_z = z_des - z(i); 
+%     del_y = y_des - y(i);
+%     
     
-    u1 = m*(g+(kp(2)+kd(2)/del_t + ki(2)*del_t)*del_z);
-    u2 =(kp(3)+kd(3)/del_t  + ki(3)*del_t)+del_phi*I_xx;
-    phi_des = -1/g*(kp(1)+kd(1)/del_t + ki(1)*del_t)*del_y;
+%     u1 = m*(g+(kp(2)+kd(2)/del_t + ki(2)*del_t)*del_z);
+%     u2 =(kp(3)+kd(3)/del_t  + ki(3)*del_t+del_phi)*I_xx;
+%     phi_des = -1/g*(kp(1)+kd(1)/del_t + ki(1)*del_t)*del_y;
+    
+    del_z = z(i) - z(i-1);
+    del_y = y(i) - y(i-1);
+    u1 = m*(g + 0 +kd(2)*(0 - del_z/del_t) + kp(2)*(z_des - z(i)));
+    phi_c = -1/g*(0 + kd(1)*(0 - del_y/del_t) + kp(1)*(y_des - y(i)));   
+    
+    del_phidot = (phi_c - phi(i-1))/del_t - (phi(i)-phi(i-1))/del_t;
+    u2 = I_xx*(0 + kd(3)*(0 - del_phidot/del_t) + kp(3)*(phi_c - phi(i)));
+%     del_phi = phi(i) - phi(i-1);
+%     del_phi_c = 
+%     
+%     
+%     
+%     
+%     
+%     prev_phi_c = phi_c;
     
     yd_dot = -g*phi(i);
     zd_dot = -g +u1/m;
